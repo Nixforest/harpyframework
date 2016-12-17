e8b264efd358419dfd331cd29c561fe0bc47a40f
SPJ <spj@spjs-mac-mini.local>
2016-12-17 21:37:41 +0700

    Create file project

---
+++ harpyframework/harpyframework/harpyframework.h
@@ -0,0 +1,19 @@
+//
+//  harpyframework.h
+//  harpyframework
+//
+//  Created by SPJ on 12/17/16.
+//  Copyright © 2016 SPJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for harpyframework.
+FOUNDATION_EXPORT double harpyframeworkVersionNumber;
+
+//! Project version string for harpyframework.
+FOUNDATION_EXPORT const unsigned char harpyframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <harpyframework/PublicHeader.h>
+
+
