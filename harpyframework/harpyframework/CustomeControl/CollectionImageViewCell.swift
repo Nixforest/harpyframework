01be66f1e21a9bf349de686d5e66230bb26a3483
tuankhoi <tuankhoi273@gmail.com>
2018-08-22 16:32:16 +0700

    BUG0203-BUG0204
    Gasservice - Redesign TicketView Screen, add Image for Reply and show
    Image for reply item
    Gasservice - Add Button Delete into CollectionView of Vip Customer
    Order Screen

--- harpyframework/harpyframework/CustomeControl/CollectionImageViewCell.swift
+++ harpyframework/harpyframework/CustomeControl/CollectionImageViewCell.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
-
-open class CollectionImageViewCell: UICollectionViewCell {
+public protocol CollectionImageDelegte{
+    func deleteImage(cell:CollectionImageViewCell)
+}
+public class CollectionImageViewCell: UICollectionViewCell {
     @IBOutlet weak public var imageView: UIImageView!
 
-    override open func awakeFromNib() {
+    @IBOutlet weak public var btnDelete: UIButton!
+    @IBAction func deleteImage(_ sender: Any) {
+        delegate?.deleteImage(cell: self)
+    }
+    public var delegate: CollectionImageDelegte?
+    /*override open func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         imageView.translatesAutoresizingMaskIntoConstraints = true
         //imageView1.frame  = CGRect(x: 0,  y: 0,  width: self.contentView.frame.size.width, height: self.contentView.frame.size.height)
         imageView.contentMode = UIViewContentMode.scaleAspectFill
-    }
+    }*/
 
 }
